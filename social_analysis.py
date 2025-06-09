5b88fcd36ccf31318084a7c9690f434acc154574
Houston Claure <houstonclaure@Houstons-MacBook-Pro-7.local>
2025-06-09 15:21:34 -0400

    prompt

--- social_analysis.py
+++ social_analysis.py
@@ -30,10 +30,10 @@ processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicun
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load image
-image = Image.open("/home/hc864/Social_VLM/images/longvid0013.jpg")
+image = Image.open("/home/hc864/Social_VLM/images/longvid0140.jpg")
 
 # Your prompt
-prompt = "Explain the image in detail. How many people are in the image? What are they doing? What is the robot doing? What is the robot's emotion? What is the robot's intention? What is the robot's goal? What is the robot's action? What is the robot's response? What is the robot's outcome? What is the robot's next action? What is the robot's next response? What is the robot's next outcome?"
+prompt = "Explain the image in detail. How many people are in the image? What are they doing? What is the robot doing? What is the robot's next outcome?"
 
 # Preprocess
 inputs = processor(images=image, text=prompt, return_tensors="pt").to(device)
