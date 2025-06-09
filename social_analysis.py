7a648aeb792c5e98dce3e287aeb503ba02d356c0
Houston Claure <houston.claure@yale.edu>
2025-06-09 15:10:56 -0400

    first try

--- social_analysis.py
+++ social_analysis.py
@@ -7,15 +7,19 @@ import torch
 model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b")
 processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b")
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
 # Load image
-image = Image.open(requests.get("Users/houstonclaure/Desktop/Social_VLM/images/longvid0013.jpg", stream=True).raw)
+image = Image.open("/home/hc864/Social_VLM/images/longvid0013.jpg")
 
 # Your prompt
 prompt = "What is the robot doing?"
 
 # Preprocess
-inputs = processor(images=image, text=prompt, return_tensors="pt").to("cuda" if torch.cuda.is_available() else "cpu")
+inputs = processor(images=image, text=prompt, return_tensors="pt").to(device)
 
 # Generate
 outputs = model.generate(**inputs, max_new_tokens=50)
 print(processor.batch_decode(outputs, skip_special_tokens=True)[0])
+
